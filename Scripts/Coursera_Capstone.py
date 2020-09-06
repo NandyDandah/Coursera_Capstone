04814a9c6496acc3382fc649b9e7ff99ef462701
Nandy <nandydandah@gmail.com>
2020-09-06 14:50:00 +0200

    Coursera_Capstone Project

--- Scripts/Coursera_Capstone.py
+++ Scripts/Coursera_Capstone.py
@@ -1,3 +1,6 @@
-Coursera_Capstone Project
+I will use this notebook for my Coursera_Capstone Project
+
 Import the pandas library as pd
 Import the Numpy library as np
+
+print"Hello Capstone Project Course"
